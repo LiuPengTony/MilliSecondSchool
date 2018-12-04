6f210b00ad2c8fc6ceb198ef8cf67789f106f49f
liupeng <15071153893@163.com>
2018-12-04 17:15:47 +0800

    数据库配置弄好了，可以开始编码了。。。

--- src/main/java/com/millisecondschool/controller/IndexController.java
+++ src/main/java/com/millisecondschool/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.millisecondschool.controller;
 
+import com.millisecondschool.dao.OrdersMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,8 +14,11 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 public class IndexController {
 
+    @Autowired
+    private OrdersMapper ordersMapper;
+
     @RequestMapping("/first")
-    public ModelAndView first(HttpServletRequest request){
+    public ModelAndView first(HttpServletRequest request) {
         ModelAndView view = new ModelAndView("index");
         return view;
     }
